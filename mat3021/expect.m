e4f2231b0efae932479d55549fe6e89a282d19b1
skkmaths <skkmaths@gmail.com>
2023-04-18 19:15:20 +0530

    add log scale

--- mat3021/expect.m
+++ mat3021/expect.m
@@ -1,5 +1,5 @@
 N = 1000000; % number of points to generate
-axis = [1:N];
+Xaxis = [1:N];
 clearvars sum
 E = zeros(1,N);
 S = 0;
@@ -13,8 +13,11 @@ fprintf('Expection = %d\n ', E(N));
 
 %fplot(@x,[0 N])
 hold on
-plot(axis,E, 'LineWidth',2, axis)
+%plot(Xaxis,E, 'LineWidth',2)
+plot(Xaxis,E, 'LineWidth',2)
 xlabel('Trials');
 ylabel('Expectation');
+set(gca, 'XScale', 'log'); % to log scale X axis
+grid on
 %xlim([0 1000]);
 %ylim([-0.4 0.8]);
